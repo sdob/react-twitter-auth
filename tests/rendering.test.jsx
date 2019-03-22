84beed12f03203633e7fbc3579e45819828a8321
sdob <sdob@cs.ucc.ie>
2019-03-22 12:34:35 +0000

    Add required props when rendering under test
    This removes some warning cruft from Jest's output.

--- tests/rendering.test.jsx
+++ tests/rendering.test.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import TwitterLogin from 'TwitterLogin';
 
 describe('TwitterLogin', () => {
+  const props = {
+    loginUrl: '',
+    requestTokenUrl: '',
+    onFailure: jest.fn(),
+    onSuccess: jest.fn(),
+  };
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<TwitterLogin/>, div);
+    ReactDOM.render(<TwitterLogin {...props} />, div);
   });
 });
