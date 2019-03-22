12d3ec14933668c8ff893d9ddd00fae4d37c4128
sdob <sdob@cs.ucc.ie>
2019-03-22 13:21:29 +0000

    Auto-fix lint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import "whatwg-fetch";
-import "url-search-params-polyfill";
-import TwitterIcon from "react-icons/lib/fa/twitter";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import 'whatwg-fetch';
+import 'url-search-params-polyfill';
+import TwitterIcon from 'react-icons/lib/fa/twitter';
 
 class TwitterLogin extends Component {
   constructor(props) {
@@ -18,23 +18,21 @@ class TwitterLogin extends Component {
 
   getHeaders() {
     const headers = Object.assign({}, this.props.customHeaders);
-    headers["Content-Type"] = "application/json";
+    headers['Content-Type'] = 'application/json';
     return headers;
   }
 
   getRequestToken() {
-    var popup = this.openPopup();
+    const popup = this.openPopup();
 
     return window
       .fetch(this.props.requestTokenUrl, {
-        method: "POST",
+        method: 'POST',
         credentials: this.props.credentials,
         headers: this.getHeaders()
       })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
+      .then(response => response.json())
+      .then((data) => {
         let authenticationUrl = `https://api.twitter.com/oauth/authenticate?oauth_token=${
           data.oauth_token
         }&force_login=${this.props.forceLogin}`;
@@ -48,7 +46,7 @@ class TwitterLogin extends Component {
         popup.location = authenticationUrl;
         this.polling(popup);
       })
-      .catch(error => {
+      .catch((error) => {
         popup.close();
         return this.props.onFailure(error);
       });
@@ -61,16 +59,16 @@ class TwitterLogin extends Component {
     const top = screen.height / 2 - h / 2;
 
     return window.open(
-      "",
-      "",
-      "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
-        w +
-        ", height=" +
-        h +
-        ", top=" +
-        top +
-        ", left=" +
-        left
+      '',
+      '',
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${
+        w
+      }, height=${
+        h
+      }, top=${
+        top
+      }, left=${
+        left}`
     );
   }
 
@@ -78,7 +76,7 @@ class TwitterLogin extends Component {
     const polling = setInterval(() => {
       if (!popup || popup.closed || popup.closed === undefined) {
         clearInterval(polling);
-        this.props.onFailure(new Error("Popup has been closed by user"));
+        this.props.onFailure(new Error('Popup has been closed by user'));
       }
 
       const closeDialog = () => {
@@ -88,27 +86,26 @@ class TwitterLogin extends Component {
 
       try {
         if (
-          !popup.location.hostname.includes("api.twitter.com") &&
-          !popup.location.hostname == ""
+          !popup.location.hostname.includes('api.twitter.com')
+          && !popup.location.hostname == ''
         ) {
           if (popup.location.search) {
             const query = new URLSearchParams(popup.location.search);
 
-            const oauthToken = query.get("oauth_token");
-            const oauthVerifier = query.get("oauth_verifier");
+            const oauthToken = query.get('oauth_token');
+            const oauthVerifier = query.get('oauth_verifier');
 
             closeDialog();
             return this.getOauthToken(oauthVerifier, oauthToken);
-          } else {
-            closeDialog();
-            return this.props.onFailure(
-              new Error(
-                "OAuth redirect has occurred but no query or hash parameters were found. " +
-                  "They were either not set during the redirect, or were removed—typically by a " +
-                  "routing library—before Twitter react component could read it."
-              )
-            );
           }
+          closeDialog();
+          return this.props.onFailure(
+            new Error(
+              'OAuth redirect has occurred but no query or hash parameters were found. '
+                  + 'They were either not set during the redirect, or were removed—typically by a '
+                  + 'routing library—before Twitter react component could read it.'
+            )
+          );
         }
       } catch (error) {
         // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
@@ -124,7 +121,7 @@ class TwitterLogin extends Component {
           this.props.loginUrl
         }?oauth_verifier=${oAuthVerifier}&oauth_token=${oauthToken}`,
         {
-          method: "POST",
+          method: 'POST',
           credentials: this.props.credentials,
           headers: this.getHeaders(),
           body: {
@@ -133,12 +130,10 @@ class TwitterLogin extends Component {
           },
         }
       )
-      .then(response => {
+      .then((response) => {
         this.props.onSuccess(response);
       })
-      .catch(error => {
-        return this.props.onFailure(error);
-      });
+      .catch(error => this.props.onFailure(error));
   }
 
   getDefaultButtonContent() {
@@ -148,7 +143,9 @@ class TwitterLogin extends Component {
 
     return (
       <span>
-        {defaultIcon} {this.props.text}
+        {defaultIcon}
+        {' '}
+        {this.props.text}
       </span>
     );
   }
@@ -181,23 +178,23 @@ TwitterLogin.propTypes = {
   dialogWidth: PropTypes.number,
   dialogHeight: PropTypes.number,
   showIcon: PropTypes.bool,
-  credentials: PropTypes.oneOf(["omit", "same-origin", "include"]),
+  credentials: PropTypes.oneOf(['omit', 'same-origin', 'include']),
   customHeaders: PropTypes.object,
   forceLogin: PropTypes.bool,
   screenName: PropTypes.string
 };
 
 TwitterLogin.defaultProps = {
-  tag: "button",
-  text: "Sign in with Twitter",
+  tag: 'button',
+  text: 'Sign in with Twitter',
   disabled: false,
   dialogWidth: 600,
   dialogHeight: 400,
   showIcon: true,
-  credentials: "same-origin",
+  credentials: 'same-origin',
   customHeaders: {},
   forceLogin: false,
-  screenName: ""
+  screenName: ''
 };
 
 export default TwitterLogin;
